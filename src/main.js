029ba459933f214245b1520b78a866c57d648aed
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-09-08 20:07:25 +0400

    started with basic drawing code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 /// handles the array of images, displaying the current generation.
+
+const WIDTH = 800,
+    HEIGHT = 800;
+
+const gridWidth = 3; //n x n grid
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    background(244);
+}
--- src/photo.js
+++ src/photo.js
@@ -1,1 +1,26 @@
 //handles the photo class, each of these guys will have a CPPN that represents its genetic material.
+
+class Photo {
+    constructor(x, y, genome = NaN) {
+        this.x = x;
+        this.y = y;
+        this.sideLength = WIDTH / gridWidth;
+        this.brain = genome;
+    }
+
+    draw() {
+        let img;
+        img = createImage(this.sideLength, this.sideLength);
+        img.loadPixels();
+
+        for (let i = 0; i < img.width; i++) {
+            for (let j = 0; j < img.height; j++) {
+                //get outputs from this.brain
+                img.set(i, j, random(0, 1));
+            }
+        }
+
+        img.updatePixels();
+        image(image, this.x, this.y);
+    }
+}
